0a5266a431112427d9c6dd5e06ba65e86ed68b88
git <git@77.242.21.187>
2019-01-01 18:39:56 +0100

    Working barebones

--- main.c
+++ main.c
@@ -3,69 +3,94 @@
 #include <stdbool.h>
 #include <X11/Xlib.h>
 #include <X11/cursorfont.h>
+#include <string.h>
 #include "game.h"
 
 int main(int argc, char* argv[])
 {
-	Display *dsp;
 	int scr;
-	Window w, chr;
-	XWindowAttributes wa;
+	Window chr;
 	XEvent xev;
 
-	int par[3] = { 5, 500, 500 };
-	dsp = XOpenDisplay(NULL);
-	if( dsp == NULL )
+	int tmp[3] = { 5, 500, 500 };
+	memcpy( defenv.par, tmp, sizeof(tmp) );
+
+	defenv.x0 = (defenv.par[1]-(2*defenv.par[0]))/3; //Specifies x value for vertical board bars
+	defenv.y0 = (defenv.par[2] - (2*defenv.par[0]))/3; //Specifies y value for horizontal board bars
+
+	defenv.dsp = XOpenDisplay(getenv("Display"));
+	if( defenv.dsp == NULL )
 	{
 		fprintf( stderr, "Cannot open display\n" );
 		exit(1);
 	}
-	scr = DefaultScreen( dsp );
-	Window rwin = RootWindow( dsp, scr );
+	scr = DefaultScreen( defenv.dsp );
+	Window rwin = RootWindow( defenv.dsp, scr );
 
-	Colormap cmap = DefaultColormap( dsp, scr );
-	XColor line, p1, p2;
-	XGCValues gcvLine, gcvP1, gcvP2;
-	GC gcLine, gcP1, gcP2;
+	Colormap cmap = DefaultColormap( defenv.dsp, scr );
+	XColor line;
+	XGCValues gcvLine;
 
-	XParseColor( dsp, cmap, "rgb:00/00/00", &line );
-	XAllocColor( dsp, cmap, &line );
+	XParseColor( defenv.dsp, cmap, "rgb:00/00/00", &line );
+	XAllocColor( defenv.dsp, cmap, &line );
 	gcvLine.foreground = line.pixel;
 	gcvLine.background = line.pixel;
-	gcLine = XCreateGC( dsp, rwin,
+	defenv.gcLine = XCreateGC( defenv.dsp, rwin,
 			GCForeground | GCBackground, &gcvLine );
-	XSetLineAttributes( dsp, gcLine, 7, LineSolid, CapButt, JoinRound );
+	XSetLineAttributes( defenv.dsp, defenv.gcLine, 7, LineSolid, CapButt, JoinRound );
 
-	w = XCreateSimpleWindow( dsp, rwin,
-			0, 0, par[1], par[2],
-			par[0], BlackPixel( dsp, scr ),
-			WhitePixel( dsp, scr ) );
-	XSelectInput( dsp, w, EnterWindowMask | KeyPressMask | ButtonPressMask );
-	XMapWindow( dsp, w );
+	defenv.w = XCreateSimpleWindow( defenv.dsp, rwin,
+			0, 0, defenv.par[1], defenv.par[2],
+			defenv.par[0], BlackPixel( defenv.dsp, scr ),
+			WhitePixel( defenv.dsp, scr ) );
+	XSelectInput( defenv.dsp, defenv.w, ExposureMask | KeyPressMask | ButtonPressMask );
+	XMapWindow( defenv.dsp, defenv.w );
 
-	int x = 0, y = 0;
-	int x0 = (par[1]-(2*par[0]))/3;
-	int y0 = (par[2] - (2*par[0]))/3;
+	int x = 0, y = 0, r, c; //Stores the mouse coordinates
+	int div = defenv.par[1]/3-defenv.par[0];
 
 	bool p = 0;
 	while( 1 )
 	{
-		XNextEvent( dsp, &xev );
+		XNextEvent( defenv.dsp, &xev );
 
-		// Get cursor coordinates
 		if( xev.type == ButtonPress )
 		{
-        		XTranslateCoordinates( dsp, w, rwin, 0, 0, &x, &y, &chr );
+			system("clear");
+        		XTranslateCoordinates( defenv.dsp, defenv.w, rwin, 0, 0, &x, &y, &chr );
   			x = xev.xbutton.x_root - x;
   			y = xev.xbutton.y_root - y;
+
+			r = getRow( x, y );
+			c = getCol( x, y );
+
+			printf("Row: %d\nCol: %d\n", r/div, c/div);
+			if( board[r/div][c/div] )
+			{
+				continue;
+			}
+
 			if( p )
 			{
-				drawO( dsp, w, gcLine, x, y, par[1]/4, x0, y0 );
+				drawX( r, c );
 			}
 			else
 			{
-				drawX(par[1]/3, par[2]/3, dsp, w, gcLine, x, y, x0, y0);
+				drawO( r, c );
 			}
+
+			board[r/div][c/div] = p+1;
+			if( check() )
+			{
+				break;
+			}
+			p = !p;
+		}
+
+		if( xev.type == Expose )
+		{
+			system("clear");
+			init();
 		}
 
       		if( xev.xkey.keycode == 9 )
@@ -73,7 +98,7 @@ int main(int argc, char* argv[])
       			break;
       		}
 
-		drawBord( dsp, w, gcLine, par, x0, y0 );
-		p = !p;
+		drawBoard();
+		dbgBoard();
 	}
 }
